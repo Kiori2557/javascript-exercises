ac800b252be6054a7b029ba19b3c8c2447b1ff96
Aung Khant Paing <akpaing99@gmail.com>
2023-06-22 15:36:16 +0630

    Edit to pass the test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (text, number) {
+  msg = "";
+  if (number < 0) {
+    return "ERROR";
+  }
+  for (let i = 0; i < number; i++) {
+    msg += text;
+  }
+  return msg;
 };
-
 // Do not edit below this line
 module.exports = repeatString;

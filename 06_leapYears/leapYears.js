c68dbce677c0b1b1d30270df6d9294b38a25d97e
Aung Khant Paing <akpaing99@gmail.com>
2023-06-23 14:09:43 +0630

    Clean code

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,18 +1,20 @@
 const leapYears = function (year) {
-  let roundYear = year / 100;
-  if (roundYear == Math.round(roundYear)) {
-    if (roundYear % 4 == 0) {
-      return true;
-    } else {
-      return false;
-    }
-  } else {
-    if (year % 4 == 0) {
-      return true;
-    } else {
-      return false;
-    }
-  }
+  // My First solution
+  // let roundYear = year / 100;
+  // if (roundYear == Math.round(roundYear)) {
+  //   if (roundYear % 4 == 0) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // } else {
+  //   if (year % 4 == 0) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // }
+  return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
 };
 
 // Do not edit below this line

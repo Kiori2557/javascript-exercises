141c0b0b18308cc2027aed40ff9fc2642882ace7
Aung Khant Paing <akpaing99@gmail.com>
2023-06-23 10:20:19 +0630

    Edit reverseString.js
    Write a function that reverse any input string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (input) {
+  reverseInput = "";
+  for (let i = input.length - 1; i >= 0; i--) {
+    reverseInput += input[i];
+  }
+  return reverseInput;
 };
 
 // Do not edit below this line

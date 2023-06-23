ba2362f5a22c32918a5ab852b7728ece8648a5a6
Aung Khant Paing <akpaing99@gmail.com>
2023-06-23 12:53:58 +0630

    Add the number in the range of the arguments

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function (first, last) {
+  let sum = 0;
+  let startInteger = first;
+  let endInteger = last;
+  if (
+    first < 0 ||
+    last < 0 ||
+    typeof first != "number" ||
+    typeof last != "number"
+  ) {
+    return "ERROR";
+  }
+  if (first > last) {
+    startInteger = last;
+    endInteger = first;
+  }
+  for (let i = startInteger; i <= endInteger; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line

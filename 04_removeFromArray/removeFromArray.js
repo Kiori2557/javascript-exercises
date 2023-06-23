3cbaab12de0def042926f2155936c0d47bf0e40e
Aung Khant Paing <akpaing99@gmail.com>
2023-06-23 12:38:12 +0630

    Edit removeFromArray.js
    Remove the other arguments of a function from the array arguments

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...removeItemArray) {
+  let filteredArray = array;
+  for (let i = 0; i < removeItemArray.length; i++) {
+    filteredArray = filteredArray.filter((item) => item !== removeItemArray[i]);
+  }
+  return filteredArray;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;

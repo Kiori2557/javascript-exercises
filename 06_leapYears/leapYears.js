ac5c66dd0380aae0214e360a664eac37bfc21466
Aung Khant Paing <akpaing99@gmail.com>
2023-06-23 13:26:52 +0630

    Write function to calculate leap leapYears
    Used nesting of if else if conditions

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  let roundYear = year / 100;
+  if (roundYear == Math.round(roundYear)) {
+    if (roundYear % 4 == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    if (year % 4 == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 };
 
 // Do not edit below this line

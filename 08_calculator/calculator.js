6f5f58b14bcbf1d8045310d52dc5264c41253335
Aung Khant Paing <akpaing99@gmail.com>
2023-07-07 13:27:35 +0630

    Passed the test

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((sum, number) => sum + number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (...numbers) {
+  return numbers.reduce((result, number) => result * number);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (times) {
+  let array = [];
+  for (let i = 0; i <= times; i++) {
+    if (i == 0) {
+      array.push(1);
+    } else {
+      array.push(i);
+    }
+  }
+  return array.reduce((result, number) => result * number);
 };
 
 // Do not edit below this line
@@ -29,5 +37,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };

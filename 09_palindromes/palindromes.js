7fa8b285beac7e217e47ab274930c981fa8ad997
Aung Khant Paing <akpaing99@gmail.com>
2023-07-08 15:41:14 +0630

    passed the test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,26 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  string = string.toLowerCase();
+  let array = string.split("");
+  function remove(word) {
+    while (array.includes(word)) {
+      array.splice(array.indexOf(word), 1);
+    }
+  }
 
+  remove("!");
+  remove(" ");
+  remove(",");
+  remove(".");
+
+  let array2 = [];
+  array.forEach((word) => {
+    array2.push(word);
+  });
+
+  array2.reverse();
+  let result = array.join("");
+  let reverseResult = array2.join("");
+  return result == reverseResult;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+  let array = [1, 1];
+  if (number < 0) {
+    return "OOPS";
+  } else if (number == 0) {
+    return array[number];
+  } else {
+    for (let i = 2; i <= number - 1; i++) {
+      let sum = array[i - 2] + array[i - 1];
+      array.push(sum);
+    }
+    return array[number - 1];
+  }
 };
 
 // Do not edit below this line

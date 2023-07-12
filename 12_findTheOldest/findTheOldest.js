5bd63932b4afe34f08895ea4d4e8e7a5d1c78064
Aung Khant Paing <akpaing99@gmail.com>
2023-07-12 09:31:26 +0630

    Pass the test

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (hi) {
+  let oldest = hi[0];
+  let oldestAge = 0;
+  hi.forEach((person) => {
+    if (!person.yearOfDeath) {
+      person.yearOfDeath = new Date().getFullYear();
+    }
+    let personAge = person.yearOfDeath - person.yearOfBirth;
+    if (personAge > oldestAge) {
+      oldest = person;
+      oldestAge = personAge;
+    }
+  });
+  return oldest;
 };
 
 // Do not edit below this line
